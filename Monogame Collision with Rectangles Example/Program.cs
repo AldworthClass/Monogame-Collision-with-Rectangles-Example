835f2748be03adc9c90abf46b539c2669fd7f6e5
Steve Aldworth <s.aldworth@tvdsb.ca>
2020-10-30 13:21:03 -0400

    Add project files.

---
+++ Monogame Collision with Rectangles Example/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Monogame_Collision_with_Rectangles_Example
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Game1())
+                game.Run();
+        }
+    }
+}
